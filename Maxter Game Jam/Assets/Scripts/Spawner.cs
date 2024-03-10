79980f0561fa33ab866ae9fa93782313d1b49f45
RSLX <rasulahmadov2011@gmail.com>
2024-03-10 17:25:06 +0400

    Спавн

--- Maxter Game Jam/Assets/Scripts/Spawner.cs
+++ Maxter Game Jam/Assets/Scripts/Spawner.cs
@@ -27,7 +27,7 @@ public class Spawner : MonoBehaviour
             {
                 Instantiate(enemy, SpawnPos.position, Quaternion.identity);
             }
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(25);
         }
     }
 }
